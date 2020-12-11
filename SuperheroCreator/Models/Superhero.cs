4eda011a73bb521f5578fa8735fb0be5879a9d6f
GRy82 <gwryan@buffalo.edu>
2020-12-11 13:52:29 -0500

    Properties declared in Superhero class

--- SuperheroCreator/Models/Superhero.cs
+++ SuperheroCreator/Models/Superhero.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,16 @@ namespace SuperheroCreator.Models
 {
     public class Superhero
     {
+        [Key]
+        public int Id { get; set; }
+        public string HeroName { get; set; }
+
+        public string CivilianName { get; set; }
+
+        public string PrimaryAbility { get; set; }
+
+        public string SecondaryAbility { get; set; }
+
+        public string CatchPhrase { get; set; }
     }
 }
